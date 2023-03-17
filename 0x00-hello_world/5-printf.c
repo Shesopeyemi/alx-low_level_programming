5ca0de1c9f047d6ec957b72bfcf700187b99f87a
Shesopeyemi <opeyemilawal640@gmail.com>
2023-03-17 03:58:27 -0700

    printing

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - printing sets of codes with the aid of printf function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar. but the outcome is a piece of art,\n");
+	return (0);
+}
