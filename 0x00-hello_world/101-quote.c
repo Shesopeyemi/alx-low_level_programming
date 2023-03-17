1a62b64bc5af159002c53d4c93909cc4846dc973
Shesopeyemi <opeyemilawal640@gmail.com>
2023-03-17 05:21:54 -0700

    low

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - printing sets of lines of codes
+ *
+ * Return: (1)
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19
+return (1);
+}
