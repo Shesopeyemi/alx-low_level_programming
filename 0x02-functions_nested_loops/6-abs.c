26cee9475fef695ec28290c8a939615f3f22b723
Shesopeyemi <opeyemilawal640@gmail.com>
2023-03-22 13:27:36 -0700

    kilo

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - program that prints value of an integer
+ *
+ * @a: parameter to be checked
+ * Return: always 0
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = -a;
+	return (a);
+}
