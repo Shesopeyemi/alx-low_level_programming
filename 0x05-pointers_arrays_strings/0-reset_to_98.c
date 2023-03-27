80a570d1bf85d51b5083fd64495689ccf7bcf90e
Shesopeyemi <opeyemilawal640@gmail.com>
2023-03-27 13:03:01 -0700

    first point

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value to 98
+ * @n: parameter to be checked
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+void reset_to_98(int *n);
+
+#endif
