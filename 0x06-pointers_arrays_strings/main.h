58f36becba745b5925991f955e8888a76b7178c9
Shesopeyemi <opeyemilawal640@gmail.com>
2023-03-30 15:07:40 -0700

    lesser

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,6 @@ void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
 #endif /*MAIN_H*/
