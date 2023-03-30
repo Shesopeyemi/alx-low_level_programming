cad6bc587fefa433676deaea172a7db3542d9a4c
Shesopeyemi <opeyemilawal640@gmail.com>
2023-03-30 14:57:25 -0700

    more is less

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+#include <stdio.h>
+/**
+ * void prototypes
+ * int prototypes
+ * char prototypes
+ */
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+#endif /*MAIN_H*/
